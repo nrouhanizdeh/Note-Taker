be0d75c14458f720cbb310682974d63ae7559888
Negin Rouhanizadeh <negin.rouhanizadeh@gmail.com>
2020-11-08 14:35:16 -0600

    added api routes and html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
+let notes = require("../db/db.json");
+const fs = require("fs");
 var path = require("path");
 var datapath = path.join(__dirname, "../db/db.json");
-var notes = require("../db/db.json");
-var fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 module.exports = function (app) {
@@ -26,11 +26,15 @@ module.exports = function (app) {
   });
 
   app.delete("/api/notes/:id", function (req, res) {
-    var filteredNote = notes.filter((note) => note.id !== req.params.id);
-    fs.writeFileSync(datapath, JSON.stringify(filteredNote), "utf8", function (
-      err
-    ) {
+    fs.readFile(datapath, function (error) {
       if (error) throw error;
+
+      var filteredNote = notes.filter((note) => note.id !== req.params.id);
+      fs.writeFileSync(datapath, JSON.stringify(filteredNote), function (
+        error
+      ) {
+        if (error) throw error;
+      });
     });
     res.json({ ok: true });
   });
